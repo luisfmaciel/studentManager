6495b23977e61f6ee0b4937e114534ffdec22081
Luis Felipe Maciel <luis.barbosa@al.infnet.edu.br>
2024-05-30 19:27:17 -0300

    Habilitando caching

--- src/main/java/br/edu/infnet/studentmanager/StudentManagerApplication.java
+++ src/main/java/br/edu/infnet/studentmanager/StudentManagerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-//@EnableCaching
+@EnableCaching
 public class StudentManagerApplication {
 
     public static void main(String[] args) {
